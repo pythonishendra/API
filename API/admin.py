86e9e413a24ca346bdb23d8f39a3e54f3efd6b5e
aswani kumar <paythonishendra.netcoreinfo@gmail.com>
2020-05-04 19:30:23 +0530

    asd

--- API/admin.py
+++ API/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Address),
-admin.site.register(MemberAddress)
+# admin.site.register(Address),
+# admin.site.register(MemberAddress)
